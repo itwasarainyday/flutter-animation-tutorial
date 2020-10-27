b992b9966abf328055198eff36510bbe3d826d78
Matthew Kwong <mkwong@tribalscale.com>
2020-10-27 19:28:16 -0400

    [MK] chore: Remove animations for LBD.

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_lbd/light_switch.dart';
-import 'package:flutter_animation_lbd/moon.dart';
 import 'package:flutter_animation_lbd/sun.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -11,23 +10,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   bool isDayTime = true;
   bool isSunUp = true;
-
-  List<Color> getBackgroundGradient() {
-    if (isDayTime) {
-      return [
-        Color(0xFF8C2480),
-        Color(0xFFCE587D),
-        Color(0xFFFF9485),
-        if (isSunUp) Color(0xFFFF9D80),
-      ];
-    } else {
-      return [
-        Color(0xFF0D1441),
-        Color(0xFF283584),
-        Color(0xFF376AB2),
-      ];
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,21 +29,12 @@ class _HomeScreenState extends State<HomeScreen> {
       children: [
         SizedBox(height: 80),
         LightSwitch(
-          onSwitched: (isOn) {
-            setState(() {
-              isDayTime = !isOn;
-            });
-            Future.delayed(Duration(milliseconds: 200), () {
-              setState(() {
-                isSunUp = !isOn;
-              });
-            });
-          },
+          onSwitched: (isOn) {},
         ),
         SizedBox(height: 20),
         Center(
           child: Text(
-            isDayTime ? "Good Morning!" : "Good Evening!",
+            "Good Morning!",
             style: TextStyle(
               fontSize: 24,
               fontWeight: FontWeight.bold,
@@ -74,33 +47,40 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildBackground() {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 1000),
-      curve: Curves.easeInOut,
+    return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          colors: getBackgroundGradient(),
+          colors: _getBackgroundGradient(),
         ),
       ),
       child: Stack(
         alignment: Alignment.center,
         children: [
-          AnimatedPositioned(
-            curve: Curves.easeInOut,
-            duration: Duration(seconds: 1),
-            bottom: isSunUp ? 150 : -300,
+          Positioned(
+            bottom: 150,
             child: Sun(),
-          ),
-          AnimatedPositioned(
-            curve: Curves.easeInOut,
-            duration: Duration(seconds: 1),
-            bottom: !isSunUp ? 150 : -300,
-            child: Moon(),
           ),
         ],
       ),
     );
+  }
+
+  List<Color> _getBackgroundGradient() {
+    if (isDayTime) {
+      return [
+        Color(0xFF8C2480),
+        Color(0xFFCE587D),
+        Color(0xFFFF9485),
+        if (isSunUp) Color(0xFFFF9D80),
+      ];
+    } else {
+      return [
+        Color(0xFF0D1441),
+        Color(0xFF283584),
+        Color(0xFF376AB2),
+      ];
+    }
   }
 }
